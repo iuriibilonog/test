58018ddee5f94991111f0bca95430b71561644b6
Iurii Bilonog <iuriibilonog@gmail.com>
2022-10-03 18:21:04 +0100

    fixed

--- src/components/MainInfoScreen/MainInfoScreen.js
+++ src/components/MainInfoScreen/MainInfoScreen.js
@@ -1,25 +1,47 @@
-import { useState } from 'react';
-import s from './MainInfoScreen.module.css';
+import { useState } from "react";
+import s from "./MainInfoScreen.module.css";
 
 const MainInfoScreen = () => {
-  const [isShowSecure, setIsShowSecure] = useState(false);
-  const [isShowIndep, setIsShowIndep] = useState(false);
-  const [isShowTransparent, setIsShowTransparent] = useState(false);
-  const [isShowExperienced, setIsShowExperienced] = useState(false);
+  const [isShowTabsInfo, setIsShowTabsInfo] = useState({
+    secure: false,
+    indep: false,
+    transparent: false,
+    experienced: false,
+  });
 
-  const handleOnClick = name => {
+  const handleOnClick = (name) => {
     switch (name) {
-      case 'secure':
-        setIsShowSecure(!isShowSecure);
+      case "secure":
+        setIsShowTabsInfo({
+          secure: !isShowTabsInfo.secure,
+          indep: false,
+          transparent: false,
+          experienced: false,
+        });
         break;
-      case 'indep':
-        setIsShowIndep(!isShowIndep);
+      case "indep":
+        setIsShowTabsInfo({
+          secure: false,
+          indep: !isShowTabsInfo.indep,
+          transparent: false,
+          experienced: false,
+        });
         break;
-      case 'transparent':
-        setIsShowTransparent(!isShowTransparent);
+      case "transparent":
+        setIsShowTabsInfo({
+          secure: false,
+          indep: false,
+          transparent: !isShowTabsInfo.transparent,
+          experienced: false,
+        });
         break;
-      case 'experienced':
-        setIsShowExperienced(!isShowExperienced);
+      case "experienced":
+        setIsShowTabsInfo({
+          secure: false,
+          indep: false,
+          transparent: false,
+          experienced: !isShowTabsInfo.experienced,
+        });
         break;
 
       default:
@@ -32,94 +54,109 @@ const MainInfoScreen = () => {
       <div className={s.titleInfoWrapper}>
         <h3 className={s.title}>WELCOME</h3>
         <p className={s.mainText}>
-          Louvre Wealth Management provides comprehensive wealth management and investment services
-          to high net worth individuals, family offices and institutional clients. Our team has a
-          longstanding track record in international private wealth management, investments and
-          corporate advisory.
+          Louvre Wealth Management provides comprehensive wealth management and
+          investment services to high net worth individuals, family offices and
+          institutional clients. Our team has a longstanding track record in
+          international private wealth management, investments and corporate
+          advisory.
         </p>
       </div>
       <ul className={s.mainList}>
-        <li className={s.secure} onClick={() => handleOnClick('secure')}>
+        <li className={s.secure} onClick={() => handleOnClick("secure")}>
           <span className={s.listItemTitle}>SECURE</span>
           <div className={s.secureTabTextWrapper}>
             <h4 className={s.textTitleForTab}>SECURE</h4>
             <p className={s.textForTab}>
-              We believe that an unbiased, objective approach to selecting investments provides the
-              greatest possibility of success. To identify the best investments, we look across a
-              broad range of opportunities.
+              We believe that an unbiased, objective approach to selecting
+              investments provides the greatest possibility of success. To
+              identify the best investments, we look across a broad range of
+              opportunities.
             </p>
           </div>
         </li>
-        {isShowSecure && (
+        {isShowTabsInfo.secure && (
           <li className={s.listItemTextWrapper}>
             <h4 className={s.listItemTextTitle}>SECURE</h4>
             <p className={s.listItemText}>
-              We believe that an unbiased, objective approach to selecting investments provides the
-              greatest possibility of success. To identify the best investments, we look across a
-              broad range of opportunities.
+              We believe that an unbiased, objective approach to selecting
+              investments provides the greatest possibility of success. To
+              identify the best investments, we look across a broad range of
+              opportunities.
             </p>
           </li>
         )}
-        <li className={s.independent} onClick={() => handleOnClick('indep')}>
+        <li className={s.independent} onClick={() => handleOnClick("indep")}>
           <span className={s.listItemTitle}>INDEPENDENT</span>
           <div className={s.independentTabTextWrapper}>
             <h4 className={s.textTitleForTab}>INDEPENDENT</h4>
             <p className={s.textForTab}>
-              We believe that an unbiased, objective approach to selecting investments provides the
-              greatest possibility of success. To identify the best investments, we look across a
-              broad range of opportunities.
+              We believe that an unbiased, objective approach to selecting
+              investments provides the greatest possibility of success. To
+              identify the best investments, we look across a broad range of
+              opportunities.
             </p>
           </div>
         </li>
-        {isShowIndep && (
+        {isShowTabsInfo.indep && (
           <li className={s.listItemTextWrapper}>
             <h4 className={s.listItemTextTitle}>INDEPENDENT</h4>
             <p className={s.listItemText}>
-              We believe that an unbiased, objective approach to selecting investments provides the
-              greatest possibility of success. To identify the best investments, we look across a
-              broad range of opportunities.
+              We believe that an unbiased, objective approach to selecting
+              investments provides the greatest possibility of success. To
+              identify the best investments, we look across a broad range of
+              opportunities.
             </p>
           </li>
         )}
-        <li className={s.transparent} onClick={() => handleOnClick('transparent')}>
+        <li
+          className={s.transparent}
+          onClick={() => handleOnClick("transparent")}
+        >
           <span className={s.listItemTitle}>TRANSPARENT</span>
           <div className={s.transparentTabTextWrapper}>
             <h4 className={s.textTitleForTab}>TRANSPARENT</h4>
             <p className={s.textForTab}>
-              We believe that an unbiased, objective approach to selecting investments provides the
-              greatest possibility of success. To identify the best investments, we look across a
-              broad range of opportunities.
+              We believe that an unbiased, objective approach to selecting
+              investments provides the greatest possibility of success. To
+              identify the best investments, we look across a broad range of
+              opportunities.
             </p>
           </div>
         </li>
-        {isShowTransparent && (
+        {isShowTabsInfo.transparent && (
           <li className={s.listItemTextWrapper}>
             <h4 className={s.listItemTextTitle}>TRANSPARENT</h4>
             <p className={s.listItemText}>
-              We believe that an unbiased, objective approach to selecting investments provides the
-              greatest possibility of success. To identify the best investments, we look across a
-              broad range of opportunities.
+              We believe that an unbiased, objective approach to selecting
+              investments provides the greatest possibility of success. To
+              identify the best investments, we look across a broad range of
+              opportunities.
             </p>
           </li>
         )}
-        <li className={s.experienced} onClick={() => handleOnClick('experienced')}>
+        <li
+          className={s.experienced}
+          onClick={() => handleOnClick("experienced")}
+        >
           <span className={s.listItemTitle}>EXPERIENCED</span>
           <div className={s.experiencedTabTextWrapper}>
             <h4 className={s.textTitleForTab}>EXPERIENCED</h4>
             <p className={s.textForTab}>
-              We believe that an unbiased, objective approach to selecting investments provides the
-              greatest possibility of success. To identify the best investments, we look across a
-              broad range of opportunities.
+              We believe that an unbiased, objective approach to selecting
+              investments provides the greatest possibility of success. To
+              identify the best investments, we look across a broad range of
+              opportunities.
             </p>
           </div>
         </li>
-        {isShowExperienced && (
+        {isShowTabsInfo.experienced && (
           <li className={s.listItemTextWrapper}>
             <h4 className={s.listItemTextTitle}>EXPERIENCED</h4>
             <p className={s.listItemText}>
-              We believe that an unbiased, objective approach to selecting investments provides the
-              greatest possibility of success. To identify the best investments, we look across a
-              broad range of opportunities.
+              We believe that an unbiased, objective approach to selecting
+              investments provides the greatest possibility of success. To
+              identify the best investments, we look across a broad range of
+              opportunities.
             </p>
           </li>
         )}
